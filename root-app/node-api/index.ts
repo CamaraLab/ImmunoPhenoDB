78b65d84a3e019250f4e7a16f85524970d94fa48
lincolnwu <lincolnwu18@gmail.com>
2024-01-25 12:06:59 -0500

    top level node file

--- root-app/node-api/index.ts
+++ root-app/node-api/index.ts
@@ -4,11 +4,15 @@ import router from './routes/db_routes.js'
 
 dotenv.config({ path: '../.env'})
 
+import cors from 'cors';
+
 const app: Express = express()
 app.use(express.json())
 
-// const NODE_PORT = process.env.NODE_PORT
-const NODE_PORT = 3000
-app.listen(NODE_PORT, () => console.log(`Server is up and running at port ${NODE_PORT}`))
+app.use(cors())
 
-app.use('/', router)
+// const NODE_PORT = process.env.NODE_PORT
+const NODE_PORT = 8080
+app.listen(NODE_PORT, () => console.log(`Node server is up and running at port ${NODE_PORT}`))
+
+app.use('/api', router)
