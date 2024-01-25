a09a8e2c7e119bfab4cb2fbf3cc1efcd6e2dcb83
lincolnwu <lincolnwu18@gmail.com>
2024-01-25 12:05:53 -0500

    express api endpoints

--- root-app/node-api/routes/db_routes.ts
+++ root-app/node-api/routes/db_routes.ts
@@ -1,11 +1,27 @@
 import express, { Express, Router, Request, Response } from 'express'
-import { idCLs, runLMM } from '../controllers/db_calls.js'
+import {idCLs, 
+        tissues,
+        findAbs, 
+        plotAbs, 
+        findCellTypes, 
+        plotCellTypes, 
+        findExperiments, 
+        whichAntibodies,
+        whichCellTypes} from '../controllers/db_calls.js'
 
 
 const router : Router = express.Router()
 
-router.get('/idCLs', idCLs)
-router.post('/abLMM', runLMM)
+router.get('/idcls', idCLs)
+router.get('/tissues', tissues)
+
+router.post('/findabs', findAbs)
+router.post('/plotabs', plotAbs)
+router.post('/findcelltypes', findCellTypes)
+router.post('/plotcelltypes', plotCellTypes)
+router.post('/findexperiments', findExperiments)
+router.post('/whichantibodies', whichAntibodies)
+router.post('/whichcelltypes', whichCellTypes)
 
 // module.exports = router;
 export default router
