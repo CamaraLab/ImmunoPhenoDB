dbd9e120f2bd24c4f8faeaa7a4abd3ff8b7921a6
lincolnwu <lincolnwu18@gmail.com>
2025-04-11 11:28:11 -0400

    node timeout to 20 min

--- root-app/node-api/index.ts
+++ root-app/node-api/index.ts
@@ -11,14 +11,14 @@ app.use(express.json());
 app.use(cors());
 
 const NODE_PORT = 8080; // You can also use process.env.NODE_PORT if needed
-const SERVER_TIMEOUT = 10 * 60 * 1000; // 10 minutes in milliseconds
+const SERVER_TIMEOUT = 20 * 60 * 1000; // 20 minutes in milliseconds
 
 // Start the server and capture the server instance
 const server = app.listen(NODE_PORT, () => {
     console.log(`Node server is up and running at port ${NODE_PORT}`);
 });
 
-// Set the server timeout to 10 minutes
+// Set the server timeout to 20 minutes
 server.timeout = SERVER_TIMEOUT;
 
 app.use('/api', router);
